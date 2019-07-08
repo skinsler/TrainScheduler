c89b8050aadb2c55034f7c40cb74efe47bcef66f
skinsler <simonkinsler@gmail.com>
2019-07-07 23:23:50 -0500

    reworked app to meet full requirements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+//setup database connection
+
 var config = {
     apiKey: "AIzaSyC3aSzIvyEroIk9OJ4zYYGCTytQw7kV6-0",
     authDomain: "train-scheduler-1ebe6.firebaseapp.com",
@@ -12,21 +14,10 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-
-let trainName = "";
-let destination = "";
-let frequency = 0;
-let firstTrainTime = 0;
-let nextArrival = 0;
-let minutesAway = 0;
-
-
 $("#submit").on("click", function(event) {
     event.preventDefault();
 
-    // YOUR TASK!!!
-    // Code in the logic for storing and retrieving the most recent user.
-    // Don't forget to provide initial data to your Firebase database.
+    //get values
     trainName = $("#train-input").val().trim();
     destination = $("#destination-input").val().trim();
     frequency = $("#frequency-input").val().trim();
@@ -41,8 +32,11 @@ $("#submit").on("click", function(event) {
         firstTrainTime: firstTrainTime
     });
 
-
-
+    //clear form
+    $("#train-input").val(""); 
+    $("#destination-input").val("");
+    $("#frequency-input").val("");
+    $("#first-train-input").val("");
   });
 
 
@@ -50,61 +44,35 @@ $("#submit").on("click", function(event) {
 
 database.ref().on("child_added", function(childSnapshot) {
 
-    // Log everything that's coming out of snapshot
-    console.log(childSnapshot.val().trainName);
-    console.log(childSnapshot.val().destination);
-    console.log(childSnapshot.val().frequency);
-    console.log(childSnapshot.val().firstTrainTime);
+   //calculate next arrival and mintues away
+
+ 
+   //push first train time back 1 year to make sure it comes before current time
+   var firstTrainTime = moment(childSnapshot.val().firstTrainTime, "hh:mm").subtract(1, "years");
+
+   var diffTime = moment().diff(moment(firstTrainTime), "minutes");
+   var timeRemainder = diffTime % (childSnapshot.val().frequency);
+   var minutesAway = (childSnapshot.val().frequency) - timeRemainder;
+   var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm");
 
 
-    //TODO : Add rows to table
 
-    // full list of items to the well
-    // $("#full-member-list").append("<div class='well'><span class='member-name'> " +
-    //     childSnapshot.val().name +
-    //     " </span><span class='member-email'> " + childSnapshot.val().email +
-    //     " </span><span class='member-age'> " + childSnapshot.val().age +
-    //     " </span><span class='member-comment'> " + childSnapshot.val().comment +
-    //     " </span></div>");
 
+
+    //Add rows to table
+
+    $("tbody").append(
+        "<tr>" +
+        "<td>" + childSnapshot.val().trainName + "</td>" +
+        "<td>" + childSnapshot.val().destination + "</td>" + 
+        "<td>" + childSnapshot.val().frequency + "</td>" + 
+        "<td>" + nextArrival + "</td>" +
+        "<td>" + minutesAway + "</td>" +  
+        "</td>"
+    )
+    
     // Handle the errors
     }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
 });  
 
-//TO DO : Complete calcuated field
-function calculateNextArrival(firstTrainTime, frequency) {
-    if ( (moment().diff(time, 'minutes') ) < 0 ) {
-        firstTrainTime = firstTrainTime.subtract(1, 'day');
-    }
-
-    var minAway = calculateMinutesAway(firstTrainTime, frequency);
-    console.log("minAway " + minAway);
-
-    var nextArrival = firstTrainTime.add(minAway, 'minutes');
-    console.log( ('Addition result' + nextArrival.format('HH:mm') ) );
-
-    return nextArrival;
-
-    
-};
-
-
-//TO DO : Complete calcuated field
-function calculateMinutesAway(firstTrainTime, frequency) {
-    var diff = moment().diff(moment(firstTrainTime), 'minutes');
-    var rem = diff % frequency;
-    
- 
-    console.log("diff " + diff + "freq" + frequency);
-    console.log(frequency - rem);
-    return (frequency - rem);
-    
-};
-
-
-//TO DO: move these calls to 'submit' click handler - calculate fields 
-var time = moment('23:50','HH:mm');
-var next = calculateNextArrival(time , 25);
-console.log("function call " + next.format('HH:mm'));
-console.log( moment(moment()).diff( time, 'minutes' ) );
